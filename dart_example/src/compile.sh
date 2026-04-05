ee438dc1031e78bc0e0e322884098ce836360699
Hedon-dev <hedon-dev@hedon-haven.top>
2026-04-05 18:51:32 +0200

    Fix wrong file being used

--- dart_example/src/compile.sh
+++ dart_example/src/compile.sh
@@ -3,7 +3,7 @@
 echo "Run from root directory of the plugin for correct output!"
 
 echo "Compiling dart code to js..."
-dart compile js ./src/lib/main.dart -O4 -o ./src/lib/compiled-dart.js
+dart compile js ./src/lib/wrapper.dart -O4 -o ./src/lib/compiled-dart.js
 
 echo "Combining into bundle.js..."
 : > bundle.js
